6a07771c3caff989523b9e53c15d733b6e3b2a51
loky <loky1118@163.com>
2020-09-26 01:16:52 +0800

    first-commit

--- src/main/scala/com/hong/apitest/WindowTest.scala
+++ src/main/scala/com/hong/apitest/WindowTest.scala
@@ -10,13 +10,20 @@ import org.apache.flink.streaming.api.windowing.windows.TimeWindow
 import org.apache.flink.util.Collector
 import com.hong.apitest.SensorReading
 import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.functions.{AssignerWithPeriodicWatermarks, AssignerWithPunctuatedWatermarks}
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor
+import org.apache.flink.streaming.api.watermark.Watermark
 
 object WindowTest {
     def main(args: Array[String]): Unit = {
         val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
         env.setParallelism(1)
+        // 点setStreamTimeCharacteristic源码可知：
+        // 当设置为EvenTime的时候，没200ms 生成一个WaterMark。
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)   // 设置事件时间
+        // 改变周期性自动生成WaterMark的时间周期
+        env.getConfig.setAutoWatermarkInterval(500L)  // 修改为每 500毫秒生成一个WaterMark
+
 
         val inputDataStream =
             env.addSource(new MySensorSource())
@@ -41,14 +48,20 @@ object WindowTest {
             //            .reduce(new MyReduce())               // 测试1：增量聚合窗口
 
             // 机制2：除了水印保证乱序数据，还有运行迟到数据的机制
-            // 先计算一次水位线前的窗口，然后允许1分钟后的迟到数据进来，再次计算，并关闭窗口。来一条迟到就计算并输出一次
+            // 先计算一次水位线前的窗口，然后允许1分钟后的迟到数据进来，再次计算，并关闭窗口。
+            // 来一条迟到就计算并输出一次
             .allowedLateness(Time.minutes(1))
-            //
+            // 机制3：侧输出流。整个窗口关闭后，还有迟到数据。
+            // 将原来已关闭的窗口聚合结果再拿出来和迟到数据计算
+            .sideOutputLateData(new OutputTag[SensorReading]("late_test"))
 
             .apply(new MyWindowFunction()) // 测试2：全窗口函数
 
         inputDataStream.print("data")
         resultStream.print("result")
+
+        // 打印迟到的侧输出流
+        resultStream.getSideOutput(new OutputTag[SensorReading]("late_test"))
 
         env.execute()
 
@@ -69,3 +82,27 @@ class MyWindowFunction() extends WindowFunction[SensorReading, Int, Tuple, TimeW
     }
 }
 
+// 自定义一个周期性生成WaterMark的Assigner。 （模拟BoundedOutOfOrdernessTimestampExtractor）
+// 看源码中：assignTimestampsAndWatermarks 的传入参数注释
+class MyWMAssigner extends AssignerWithPeriodicWatermarks[SensorReading] {
+
+    // 需要两个关键参数：延迟时间和当前数据的最大时间戳
+    val lateness: Long = 1000L
+    var maxTs: Long = Long.MinValue
+
+    override def getCurrentWatermark: Watermark =
+        new Watermark(maxTs - lateness)
+
+    // 每来一条数据，就会调用一次extractTimestamp方法
+    override def extractTimestamp(element: SensorReading, previousElementTimestamp: Long): Long = {
+        maxTs = maxTs.max(element.timestamp * 1000L)
+        element.timestamp * 1000L
+    }
+}
+
+// 自定义一个断点式生成WaterMark的Assigner
+class MyWMAssigner2 extends AssignerWithPunctuatedWatermarks[SensorReading] {
+    override def checkAndGetNextWatermark(lastElement: SensorReading, extractedTimestamp: Long): Watermark = ???
+
+    override def extractTimestamp(element: SensorReading, previousElementTimestamp: Long): Long = ???
+}
